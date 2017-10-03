730d0f1e9fb4932964807f8c5bf6f19860a7d7d5
cmirza <6oveWAytG9Hmvd>
2017-10-03 12:11:16 -0700

    initial revision

---
+++ python/lab1-turtle.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+#draw body
+forward(200)
+left(180)
+forward(100)
+left(90)
+forward(200)
+left(45)
+forward(100)
+left(180)
+forward(100)
+left(90)
+forward(100)
+left(180)
+forward(100)
+left(45)
+forward(240)
+
+# draw head
+right(90)
+circle(50)
+
+done()
