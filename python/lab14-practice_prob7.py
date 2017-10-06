834e8b0285c34ad6f52734275d7784d48cc22a4f
cmirza <6oveWAytG9Hmvd>
2017-10-06 11:06:42 -0700

    initial revision

---
+++ python/lab14-practice_prob7.py
@@ -0,0 +1,34 @@
+'''
+Lab 14 - Practice Problems
+Problem 7: Write functions to find the minimum, maximum, mean, and (optionally) mode of a list of numbers.
+'''
+
+
+def minimum(nums):  # define function
+    min_value = 0  # smallest number in list so far
+    for n in nums:  # start loop
+        if n < min_value:  # if selected number in list is smaller than smallest number in list so far set it as smallest number
+            min_value = n
+    return n  # return the smallest number in list
+
+
+def maximum(nums):  # define function
+    max_value = 0  # largest number in list so far
+    for n in nums:  # start loop
+        if n > max_value:  # if selected number in list is greater than largest number in list so far set it as largest number
+            max_value = n
+    return n  # return the largest number in list
+
+
+def mean(nums):  # define function
+    sum = 0  # sum of number in list
+    for n in nums:  # start loop
+        sum = sum + n  # add up numbers in list
+    return sum/len(nums)  # divide sum of list by number of items in list and return number
+
+
+values = [1, 12, 2, 53, 23, 6, 17]  # list values
+
+print("Largest number in list: ", minimum(values), "\n")
+print("Smallest number in list: ", maximum(values), "\n")
+print("Mean of numbers in list: ", mean(values), "\n")
---
+++ python/lab14-practice_prob8.py
@@ -0,0 +1,16 @@
+'''
+Lab 14 - Practice Problems
+Problem 8: Write a function that returns the reverse of a list.
+'''
+
+
+def reverse(nums):  #define reverse funcion
+    rev_nums = []  # define reverse list
+    for num in nums:  # start for loop
+        rev_nums.insert(0, num)  # use insert method to insert each number in sequence into the 0 position of reversed list
+    print(rev_nums)
+
+
+values = [1, 12, 2, 53, 23, 6, 17]
+
+reverse(values)
