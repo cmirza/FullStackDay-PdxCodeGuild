a1aec892b54ee569dde6a30166a0d9229ef606c6
cmirza <mail@cmirza.com>
2017-10-11 14:26:16 -0700

    initial revision

--- python/lab21-peaks_and_valleys_v1.py
+++ python/lab21-peaks_and_valleys_v1.py
@@ -6,9 +6,9 @@ Lab 21 - Peaks and Valleys
 # define peaks function
 def peaks(num):
     peak_list = []  # define peak list
-    for i in range(1, len(num) - 1):
-        if (num[i] > num[i - 1]) & (num[i] > num[i + 1]):
-            peak_list.append(i)
+    for i in range(1, len(num) - 1):  # run through list for the length of the list
+        if (num[i] > num[i - 1]) and (num[i] > num[i + 1]):  # if the selected number is greater than previous number and its greater than the next number, add it to list
+            peak_list.append(i)  # return list
     return peak_list
 
 
@@ -16,21 +16,21 @@ def peaks(num):
 def valleys(num):
     valley_list = []  # define valley list
     for i in range(1, len(num) - 1):
-        if (num[i] < num[i - 1]) & (num[i] < num[i + 1]):
-            valley_list.append(i)
+        if (num[i] < num[i - 1]) and (num[i] < num[i + 1]):  # if the selected number is less than previous number and less than next number, add it to list
+            valley_list.append(i)  # return list
     return valley_list
 
 
 # define peaks and valleys function
 def peaks_and_valleys(num):
     peak_and_valley_list = []  # define combined peak
-    for i in range(1, len(num) - 1):
-        if (num[i] > num[i - 1]) & (num[i] > num[i + 1]):
+    for i in range(1, len(num) - 1):  # run through list for the length of the list
+        if (num[i] > num[i - 1]) and (num[i] > num[i + 1]):  # if the selected number is greater than previous number and its greater than the next number, add it to list
             peak_and_valley_list.append(i)
     for i in range(1, len(num) - 1):
-        if (num[i] < num[i - 1]) & (num[i] < num[i + 1]):
+        if (num[i] < num[i - 1]) and (num[i] < num[i + 1]):  # if the selected number is less than previous number and less than next number, add it to list
             peak_and_valley_list.append(i)
-    return sorted(peak_and_valley_list)
+    return sorted(peak_and_valley_list)  # return sorted list
 
 
 # define list of numbers
--- python/lab21-peaks_and_valleys_v3.py
+++ python/lab21-peaks_and_valleys_v3.py
@@ -1,37 +1,37 @@
 '''
 Lab 21 - Peaks and Valleys
-Draw image of Xs using data list
+Calculate accumulated 'water'.
 '''
 
 
 # define peaks function
 def peaks(num):
     peak_list = []  # define peak list
-    for i in range(1, len(num) - 1):  # run through list for the length of the list
-        if (num[i] > num[i - 1]) and (num[i] > num[i + 1]):  # if the selected number is greater than previous number and its greater than the next number, add it to list
+    for i in range(1, len(num) - 1):
+        if (num[i] > num[i - 1]) & (num[i] > num[i + 1]):
             peak_list.append(i)
-    return peak_list  # return list
+    return peak_list
 
 
 # define valleys function
 def valleys(num):
     valley_list = []  # define valley list
-    for i in range(1, len(num) - 1):  # run through list for the length of the list
-        if (num[i] < num[i - 1]) and (num[i] < num[i + 1]):  # if the selected number is less than previous number and less than next number, add it to list
+    for i in range(1, len(num) - 1):
+        if (num[i] < num[i - 1]) & (num[i] < num[i + 1]):
             valley_list.append(i)
-    return valley_list  # return list
+    return valley_list
 
 
 # define peaks and valleys function
 def peaks_and_valleys(num):
     peak_and_valley_list = []  # define combined peak
-    for i in range(1, len(num) - 1):  # run through list for the length of the list
-        if (num[i] > num[i - 1]) and (num[i] > num[i + 1]):  # if the selected number is greater than previous number and its greater than the next number, add it to list
+    for i in range(1, len(num) - 1):
+        if (num[i] > num[i - 1]) & (num[i] > num[i + 1]):
             peak_and_valley_list.append(i)
     for i in range(1, len(num) - 1):
-        if (num[i] < num[i - 1]) and (num[i] < num[i + 1]):  # if the selected number is less than previous number and less than next number, add it to list
+        if (num[i] < num[i - 1]) & (num[i] < num[i + 1]):
             peak_and_valley_list.append(i)
-    return sorted(peak_and_valley_list)  # return sorted list
+    return sorted(peak_and_valley_list)
 
 
 # define ascii barchart function
