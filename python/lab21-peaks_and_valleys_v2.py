6a58a628df0fac569f081da266fc18e81f953cd9
cmirza <mail@cmirza.com>
2017-10-11 14:04:39 -0700

    initial revision

--- python/lab21-peaks_and_valleys_v2.py
+++ python/lab21-peaks_and_valleys_v2.py
@@ -34,10 +34,24 @@ def peaks_and_valleys(num):
     return sorted(peak_and_valley_list)
 
 
+# define ascii barchart function
+def ascii_barchart(num):
+    for x in range(max(num), 0, -1):  # start loop for x coord
+        print(end=' ')  # add extra space for alignment
+        for y in num:  # start loop for drawing y coord
+            if y >= x:  # if value of y is greater than or equal to x, print 'X' and suppress newline with space
+                print("X ", end=' ')
+            else:  # otherwise print ' '
+                print("  ", end=' ')
+        print()  # print newline
+    print(num)  # print number list
+
+
 # define list of numbers
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 # pass list to functions and print results
+ascii_barchart(data)
 print(peaks(data))
 print(valleys(data))
 print(peaks_and_valleys(data))
