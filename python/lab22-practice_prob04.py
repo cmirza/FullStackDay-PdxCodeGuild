a60fcbe6e6c5bfdd92546e0a4e7df85f36672526
cmirza <mail@cmirza.com>
2017-10-12 10:12:28 -0700

    initial revision

--- python/lab22-practice_prob04.py
+++ python/lab22-practice_prob04.py
@@ -4,4 +4,14 @@ Problem 4: Get a string from the user, print out another string, doubling every 
 '''
 
 
+def char_doublr(input):
 
+    input_double = ""
+    for i in range (len(input)):
+        input_double += input[i]*2
+    print(input_double)
+
+
+my_input = input('Enter some text: ')
+
+char_doublr(my_input)
