24616c4f43acceea366f62eb2c19affd54e42533
cmirza <mail@cmirza.com>
2017-10-13 10:50:01 -0700

    initial revision

--- python/lab22-practice_prob03.py
+++ python/lab22-practice_prob03.py
@@ -6,17 +6,19 @@ Problem 3: Given a list of numbers, and a target number, find a pair of numbers 
 
 def find_pair(nums, target):  # define find_pair function
 
-    chkd_nums = []  # define list of checked numbers
+    chkd_nums = []  # define list of checked numbers, prevents duplicate sets
+    pair_nums = []  # define list of pairs that sum to target
 
     for num in nums:  # run through list of numbers
         diff = target - num  # subtract number from target sum, assign to variable for difference
         if diff in chkd_nums:  # if the difference is in the list of check numbers, return number and difference number
-            return num, diff
+            pair_nums.append([num, diff])
         else:
             chkd_nums.append(num)  # add number to checked number list
+    return pair_nums  # return list of pairs that sum to target
 
 
-nums_list = [4, 6, 2, 3]  # define number list
+nums_list = [4, 6, 2, 3, 5, 1]  # define number list
 
 target_num = 7  # define target number
 
--- python/lab23-blackjack_advice.py
+++ python/lab23-blackjack_advice.py
@@ -2,25 +2,28 @@
 Lab 23 - Blackjack I
 '''
 
+# catalog of values for each card
 card_values = {'a': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'j': 10, 'q': 10, 'k': 10}
 
 
+# function for black jack advice
 def black_jack(crd1, crd2, crd3):
-    crd_total = (card_values[crd1] + card_values[crd2] + card_values[crd3])
+    crd_total = (card_values[crd1] + card_values[crd2] + card_values[crd3])  # calculate total value of cards
     print(crd_total)
 
-    if crd_total > 21:
+    if crd_total > 21:  # if total value is over 21, print already busted
         print("Already busted")
-    elif crd_total == 21:
+    elif crd_total == 21:  # if total value is 21, print 'Blackjack'
         print("Blackjack!")
-    elif crd_total >= 17:
+    elif crd_total >= 17:  # if total value is greater than or equal to 17, print 'stay'
         print("Stay")
-    elif crd_total < 17:
+    elif crd_total < 17:  # if total value is under 17, print 'hit'
         print("Hit")
 
 
+# user input loop
 while True:
-    card_1 = input('What\'s your first card? ')
+    card_1 = input('What\'s your first card? ')  # if
     if card_1 == 'done':
         break
     elif card_1 not in card_values:
