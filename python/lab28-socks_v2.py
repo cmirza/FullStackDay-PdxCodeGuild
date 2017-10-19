972891ec51cf58a101208a6990ea3be973025c4d
cmirza <mail@cmirza.com>
2017-10-19 10:22:31 -0700

    initial revision

--- python/lab28-socks_v2.py
+++ python/lab28-socks_v2.py
@@ -1,10 +1,13 @@
 '''
 Lab 28 - Socks
+Mix of types and colors
 '''
 
 import random  # import random module
 
 sock_types = ['ankle', 'crew', 'calf', 'thigh']  # list of possible socks
+
+sock_colors = ['black', 'white', 'blue']  # list of possible colors
 
 my_socks = {}  # dictionary of socks you have
 
@@ -13,13 +16,14 @@ sock_matches = {}  # dictionary of sock matches
 sock_loners = {}  # dictionary of sock loners
 
 
-# Run a loop 100 times for each sock, randomly picking a type of sock and iterating the total for that sock
+# Run a loop 100 times for each sock, randomly picking a type and color of sock. Then iterate for that combo or add
+# combo to dictionary
 for socks in range(100):
-    sock = random.choice(sock_types)
-    if sock in my_socks:
-        my_socks[sock] += 1
+    sock_type = ((random.choice(sock_colors)), (random.choice(sock_types)))
+    if sock_type in my_socks:
+        my_socks[sock_type] += 1
     else:
-        my_socks[sock] = 1
+        my_socks[sock_type] = 1
 
 # Run a loop over the dictionary of socks owned, do floor division on total to get sock matches and assign value in
 # sock matches dictionary, then do modulus on total to get sock loners and assign value in sock loners dictionary
